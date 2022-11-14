fb0302c74bfab102a82056702ea1ac01a1c360f2
Bean <bddang@svsu.edu>
2022-11-14 18:11:08 -0500

    adding more activity for initial start

---
+++ app/src/main/java/com/example/sensorgame/StartPage.java
@@ -0,0 +1,14 @@
+package com.example.sensorgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_page);
+    }
+}
