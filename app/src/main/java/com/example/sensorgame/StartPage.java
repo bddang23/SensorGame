411e48bead073b2f95e10265e3a8a046b591943b
Binh Dang <70655926+bddang23@users.noreply.github.com>
2022-11-18 12:55:41 -0500

    base starting point (#1)

--- app/src/main/java/com/example/sensorgame/StartPage.java
+++ app/src/main/java/com/example/sensorgame/StartPage.java
@@ -2,13 +2,32 @@ package com.example.sensorgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class StartPage extends AppCompatActivity {
 
+    Button btnStart;
+    Button btnResetStats;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_page);
+
+        btnStart = findViewById(R.id.btnStart);
+        btnResetStats = findViewById(R.id.btnReset);
+
+        btnStart.setOnClickListener(v->{
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        btnResetStats.setOnClickListener(v->{
+            ResetStats();
+        });
+    }
+
+    private void ResetStats() {
     }
 }
--- app/src/main/java/com/example/sensorgame/result.java
+++ app/src/main/java/com/example/sensorgame/result.java
@@ -2,13 +2,41 @@ package com.example.sensorgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class result extends AppCompatActivity {
+    TextView txtYourTime;
+    TextView txtYourHeight;
+    TextView txtBestTime;
+    TextView txtBestHeight;
 
+    Button btnReturn;
+    Button btnReplay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result_activity);
+
+        txtYourTime = findViewById(R.id.txtYourTime);
+        txtYourHeight = findViewById(R.id.txtYourHeight);
+        txtBestTime = findViewById(R.id.txtBestTime);
+        txtBestHeight = findViewById(R.id.txtBestHeight);
+
+        btnReturn =findViewById(R.id.btnReturn);
+        btnReplay =findViewById(R.id.btnReplay);
+
+        btnReturn.setOnClickListener(view -> {
+            Intent intent = new Intent(this, StartPage.class);
+            startActivity(intent);
+        });
+        btnReplay.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
